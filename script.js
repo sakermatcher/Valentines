61538808b7fe4c94d1617d3c0cc2d66aa87a206b
sakermatcher <75508552+sakermatcher@users.noreply.github.com>
2024-02-04 15:49:36 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var noClicks= 0;
+
+function yes() {
+    //Add inLove to love classes
+    document.querySelector('.background').innerHTML = '<css-doodle class="inLove" use="var(--rule)"></css-doodle>';
+    document.querySelector('.question').innerHTML =  '¡EXCELENTE!   TE AMOOOOO EVA'
+    document.querySelector('.NOPE').innerHTML= ''
+    document.querySelector('.SIU').innerHTML= '<img src="amor.jpeg" alt="amor" class="amor">'
+}
+
+function no() {
+    document.querySelector('.NOPE').innerHTML= '<button onclick="no()" style= "left: '+ (Math.random() * 90) + 'vw;top: '+ (Math.random() * 65 + 25) + 'vh;" class="noBtn">NO</button>';
+    noClicks++;
+    if (noClicks > 4) {
+        document.querySelector('.NOPE').innerHTML= '<p class=noBtn>YA NO PUEDES PICAR MAS ESTE BOTON. PON QUE SI!</p>'
+    }
+}
